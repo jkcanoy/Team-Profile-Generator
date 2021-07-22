ed339ffa701c55a51163995435fb361984ae5ac8
Kyle Canoy <kcanoy@uw.edu>
2021-07-22 13:53:31 -0700

    create package.json, create test for Employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,47 @@
+const { it, expect } = require("@jest/globals");
+const { describe } = require("yargs");
+const Employee = require("../lib/Employee.js");
+
+describe("Employee", () => {
+  describe("Init", () => {
+    it("Should instantiate Employee instance with name, id, and email", () => {
+      const E = new Employee("Kyle", 1, "kyle@kyle.com");
+
+      expect(E.name).toEqual("Kyle");
+      expect(E.id).toEqual(1);
+      expect(E.email).toEqual("kyle@kyle.com");
+    });
+  });
+
+  describe("getName", () => {
+    it("Should return Employee name from getName()", () => {
+      const E = new Employee("Kyle", 1, "kyle@kyle.com");
+
+      expect(E.getName()).toEqual("Kyle");
+    });
+  });
+
+  describe("getId", () => {
+    it("Should return Employee id from getId()", () => {
+      const E = new Employee("Kyle", 1, "kyle@kyle.com");
+
+      expect(E.getId()).toEqual(1);
+    });
+  });
+
+  describe("getEmail", () => {
+    it("Should return Employee email from getEmail()", () => {
+      const E = new Employee("Kyle", 1, "kyle@kyle.com");
+
+      expect(E.getEmail()).toEqual("kyle@kyle.com");
+    });
+  });
+
+  describe("getRole", () => {
+    it("Should return Employee role from getRole()", () => {
+      const E = new Employee("Kyle", 1, "kyle@kyle.com");
+
+      expect(E.getRole()).toEqual("Employee");
+    });
+  });
+});

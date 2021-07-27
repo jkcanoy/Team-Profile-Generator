40355824fb1391b7ef09ff2120af2d1e795dc75c
Kyle Canoy <kcanoy@uw.edu>
2021-07-26 21:48:27 -0700

    finish inquirer prompts

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { functionExpression } = require("@babel/types");
 
 const employees = [];
 
@@ -45,5 +46,37 @@ addMember = () => {
       } else {
         roleOther = "school name";
       }
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: `What is the team member's ${roleOther}`,
+            name: "roleOther",
+          },
+          {
+            type: "list",
+            message: "Would you like to add another team member?",
+            name: "addMore",
+            choices: ["Yes", "No"],
+          },
+        ])
+        .then(function ({ roleOther, addMore }) {
+          let newMember;
+          if (role === "Manager") {
+            newMember = new Manager(name, id, email, officeNumber);
+          } else if (role === "Engineer") {
+            newMember = new Engineer(name, id, email, github);
+          } else {
+            newMember = new Intern(name, id, email, school);
+          }
+          employees.push(newMember);
+          addhtml(newMember).then(function () {
+            if (addMore === "Yes") {
+              addMember;
+            } else {
+              finishHTML();
+            }
+          });
+        });
     });
 };
